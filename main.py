41c9635721b535fda44523eb44127dce04505c94
ashley7295 <avkellerman@yahoo.com>
2020-09-17 14:47:51 -0500

    removed old code in add_book

--- main.py
+++ main.py
@@ -33,12 +33,7 @@ def create_menu():
 
 def add_book():
     new_book = ui.get_book_info()
-    books = store.get_all_books()
-    
-    if new_book not in books:
-        new_book.save()
-    else:
-        print ("This book has already been added")
+    new_book.save()
     
 
 def show_read_books():
