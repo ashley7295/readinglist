306eb6656d5ac5c5e288f1af96d84f5393374391
ashley7295 <avkellerman@yahoo.com>
2020-09-17 15:48:31 -0500

    added blank lines when printing book list in ui.py

--- ui.py
+++ ui.py
@@ -28,9 +28,9 @@ def show_books(books):
 
     if books:
         for book in books:
-            print(book)
+            print('\n',book,'\n')
     else:
-        print('No books to display')
+        print('\n','No books to display','\n')
 
 
 def get_book_info():
