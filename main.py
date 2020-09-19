d0f68dad5d8e90963fa65ee8ea43c543cdc410ff
quinz <quinzel0325@gmail.com>
2020-09-19 11:42:20 -0500

    delete does not work yet

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ def create_menu():
     menu.add_option('4', 'Show Read Books', show_read_books)
     menu.add_option('5', 'Show All Books', show_all_books)
     menu.add_option('6', 'Change Book Read Status', change_read)
+    menu.add_option('7', 'Delete Book', delete_book)
     menu.add_option('Q', 'Quit', quit_program)
 
     return menu
@@ -76,6 +77,18 @@ def change_read():
 def quit_program():
     ui.message('Thanks and bye!')
 
+def delete_book():
+
+    try:
+        book_id = ui.get_book_id()
+        book = store.get_book_by_id(book_id)
+       # store._delete_book(book_id)
+        Book.delete(book_id)
+       # store.delete(book_id)
+        print(book + ' has been deleted')
+    except:
+        print('Error occured while trying to delete book')
+    #print(book + ' has been deleted')
 
 if __name__ == '__main__':
     main()
